e3eb34774f8330a58fb66402cfa76674cf099f98
Pro <chepikov.boris@gmail.com>
2019-12-20 21:13:54 +0300

    Test2

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -2,15 +2,15 @@ package ru.progwards.java1.lessons.basics;
 
 public class AccuracyDoubleFloat {
     public static void main(String[] args) {
-//        System.out.println(calculateAccuracy(6371.2));
+        System.out.println(calculateAccuracy(6371.2));
     }
     public static double volumeBallDouble(double radius) {
-        return (double) 4 / 3 * 3.14 * (radius * radius * radius);
+        return (double) (4 / 3 * 3.14) * (radius * radius * radius);
     }
     public static float  volumeBallFloat(float radius) {
-        return (float) (4 / 3 * 3.14 * (radius * radius * radius));
+        return (float) ((4 / 3 * 3.14) * (radius * radius * radius));
     }
     public static double calculateAccuracy(double radius) {
-        return (double) volumeBallDouble(radius) - volumeBallFloat((float) radius);
+        return volumeBallDouble(radius) - volumeBallFloat((float) radius);
     }
 }
