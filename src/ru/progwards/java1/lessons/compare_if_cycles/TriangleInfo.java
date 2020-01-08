7967882ea2bbce7b7cc56b0169b97438edd57a12
Pro <chepikov.boris@gmail.com>
2020-01-08 20:49:20 +0300

    Домашнее задание 5

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -8,7 +8,11 @@ public class TriangleInfo {
         c = isIsoscelesTriangle(6, 7, 10);
     }
     public static boolean isTriangle(int a, int b, int c) {
-        if (a <= b + c && b <= a + c && c <= a + b) {
+        if (a < b + c) {
+            return true;
+        } else if (b < a + c) {
+            return true;
+        } else if (c < a + b) {
             return true;
         } else {
             return false;
