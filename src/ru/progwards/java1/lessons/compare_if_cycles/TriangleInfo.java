745d8763a6285655bdff76b4fb726be446a63c5a
Pro <chepikov.boris@gmail.com>
2020-01-08 20:51:09 +0300

    Обучение =)

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -2,10 +2,6 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleInfo {
     public static void main(String[] args) {
-        boolean a, b, c;
-//        a = isTriangle(15, 8, 9);
-//        b = isRightTriangle(4, 3, 3);
-        c = isIsoscelesTriangle(6, 7, 10);
     }
     public static boolean isTriangle(int a, int b, int c) {
         if (a < b + c) {
