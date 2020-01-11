112c59f2b4ca7249e6450211437d7c689b3dba15
Pro <chepikov.boris@gmail.com>
2020-01-11 15:33:37 +0300

    Обучение =)

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -17,7 +17,7 @@ public class Eratosthenes {
     }
 
     private void sift() {
-        for (int i = 2; i < sieve.length - 1; i++) {
+        for (int i = 0; i < sieve.length - 1; i++) {
             if (i % 2 == 0) {
                 sieve[i] = false;
             }
