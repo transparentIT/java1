56dfec8c454fffa04363e9b456444984b3c9067a
Pro <chepikov.boris@gmail.com>
2020-01-11 19:03:16 +0300

    Обучение =)

--- src/test_6_3_3/Vopros2.java
+++ src/test_6_3_3/Vopros2.java
@@ -14,10 +14,14 @@ public class Vopros2 {
 //        Arrays.sort(a4);
 //        System.out.println(Arrays.equals(a3, a4));
 
-        int[] a1 = {1, 1, 1, 1, 3};
-        int[] a2 = new int[5];
-        Arrays.fill(a2, 1);
-        a2[4] = 3;
-        System.out.println(Arrays.equals(a1, a2));
+//        String str1 = new String("12345");
+//        String str2 = new String("12345");
+//        System.out.println(str1 == str2);
+
+//        int[] a1 = {1, 1, 1, 1, 3};
+//        int[] a2 = new int[5];
+//        Arrays.fill(a2, 1);
+//        a2[4] = 3;
+//        System.out.println(Arrays.equals(a1, a2));
     }
 }
