612ccf1e63e9b575c2cb6701cbef9fe18f0fbbde
Pro <chepikov.boris@gmail.com>
2020-01-11 11:21:03 +0300

    Обучение =)

--- src/test_6_1_1/sumArrayItems.java
+++ src/test_6_1_1/sumArrayItems.java
@@ -1,11 +1,11 @@
 package test_6_1_1;
 
 public class sumArrayItems {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         int[] a = {2, 4, 6, 12};
         int i = sumArrayItems(a);
     }
-    public int sumArrayItems(int[] a) {
+    public static int sumArrayItems(int[] a) {
         int k = 0;
         for (int i = 0; i < a.length; i++) {
             k = k + a[i];
