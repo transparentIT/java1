ab0533d705e390cbbebc4d2efa7c13faa07c3c14
Pro <chepikov.boris@gmail.com>
2020-01-13 20:51:24 +0300

    Обучение =)

---
+++ src/test_7_2_2/Vopros1.java
@@ -0,0 +1,10 @@
+package test_7_2_2;
+
+public class Vopros1 {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 0;
+        int x = i << 1;
+        System.out.println(x);
+    }
+}
---
+++ src/test_7_2_2/Vopros3.java
@@ -0,0 +1,8 @@
+package test_7_2_2;
+
+public class Vopros3 {
+    public static void main(String[] args) {
+        //int result = value & 1;
+    }
+
+}
