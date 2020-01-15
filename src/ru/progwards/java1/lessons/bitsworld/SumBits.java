59403d3fbf488e26ad5ffda741c3f5f9a5e13ea6
Pro <chepikov.boris@gmail.com>
2020-01-15 08:19:27 +0300

    Обучение =)

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -2,12 +2,16 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class SumBits {
     public static void main(String[] args) {
-        byte b = 5;
-        int i = sumBits(b);
+        int b = -128;
+        byte b1 = (byte) b;
+        int i = sumBits(b1);
         System.out.println(i);
     }
     public static int sumBits(byte value) {
-        String bin = String.format("%8s", Integer.toBinaryString(value)).replace(' ', '0');
+        int value1 = value;
+        int value2 = value & 0b0000000_0000000_0000000_11111111;
+        String bin = Integer.toBinaryString(value2);
+
         int j = 0;
         char[] chars = bin.toCharArray();
         for (int i = 0; i < chars.length; i++) {
