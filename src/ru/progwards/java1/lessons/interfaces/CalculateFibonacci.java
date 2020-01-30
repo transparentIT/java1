c877b8cb7533ac1b82286aa24b613c2779676315
Pro <chepikov.boris@gmail.com>
2020-01-30 22:32:14 +0300

    Обучение =)

---
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -0,0 +1,45 @@
+package ru.progwards.java1.lessons.interfaces;
+
+public class CalculateFibonacci {
+    private static CacheInfo lastFibo;
+    public static int fiboNumber(int n) {
+        if (n == lastFibo.n) {
+            return lastFibo.fibo;
+        }
+        int n0 = 1;
+        int n1 = 1;
+        int n2 = 0;
+        for(int i = 3; i <= n; i++){
+            n2 = n0 + n1;
+            n0 = n1;
+            n1 = n2;
+        }
+        if (n == 1 || n == 2) {
+            CacheInfo cacheInfo = new CacheInfo();
+            cacheInfo.n = n;
+            cacheInfo.fibo = 1;
+            lastFibo = cacheInfo;
+            return 1;
+        } else {
+            CacheInfo cacheInfo = new CacheInfo();
+            cacheInfo.n = n;
+            cacheInfo.fibo = n2;
+            lastFibo = cacheInfo;
+            return n2;
+        }
+    }
+
+    public static class CacheInfo {
+        public int n;
+        public int fibo;
+    }
+
+    public static CacheInfo getLastFibo() {
+        return lastFibo;
+    }
+
+    public static void clearLastFibo() {
+        lastFibo = null;
+    }
+}
+
